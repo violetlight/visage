b6af8533bfc5b51801b45dda20cc84e4d4ff89aa
JF <jasonhfields@gmail.com>
2015-07-18 12:33:44 -0500

    Dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/sign-up', function(req, res) {
+  var user = new User(req.body);
+  user.save(function(err, user) {
+    res.render('dashboard', {
+      user: user
+    })
+  });
+});
+
 router.post('/sign-in', function(req, res) {
-  res.send(req.body);
+  User.findOne(req.body, function(err, user) {
+    console.log(err, user);
+    if (err) res.send(err);
+    res.render('dashboard', {
+      user: user
+    });
+  });
 });
 
 module.exports = router;
