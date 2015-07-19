2d00ff0242ab23fc54568858a5725ba35b518a51
JF <jasonhfields@gmail.com>
2015-07-18 23:11:42 -0500

    Simplified

--- passport.js
+++ passport.js
@@ -6,12 +6,8 @@ passport.use(new LocalStrategy(
   function(username, password, done) {
     User.findOne({ username: username }, function(err, user) {
       if (err) return done(err);
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validatePassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
+      if (!user) return done(null, false);
+      if (!user.verifyPassword(password)) return done(null, false);
       return done(null, user);
     });
   }
