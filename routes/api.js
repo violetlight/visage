4b4759b820615c44369963a2570e1397ac96bde3
JF <jasonhfields@gmail.com>
2015-07-19 16:33:31 -0500

    Properly-working file upload endpoint

--- routes/api.js
+++ routes/api.js
@@ -3,13 +3,12 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
-var upload = multer({ dest: 'uploads/' });
+var upload = multer({ dest: './uploads/' });
 
-router.post('/upload', upload.single('avatar'),
-  function (req, res, next) {
-    // create Image object with req.file.url or whatever here
-    res.send("uploaded file");
-  }
-);
+router.post("/upload", upload.single('file'), function(req, res) {
+  console.dir(req.file);
+  res.send("File uploaded successfully");
+});
+
 
 module.exports = router;
