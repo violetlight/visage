38a4667d7887e8d82258780c8ea72b80d2d97972
JF <jasonhfields@gmail.com>
2015-07-18 23:12:29 -0500

    Some routes

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,23 @@ var passport = require('../passport');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if (req.user) res.redirect('/dashboard');
-  res.render('index');
+  res.render('sign-up');
 });
 
 router.get('/dashboard', function(req, res) {
+  if (!req.user) res.redirect('/');
   res.render('dashboard', {
     user: req.user
   });
 });
 
-router.post('/sign-up', function(req, res) {
+router.get('/dashboard/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+
+router.post('/', function(req, res) {
   var user = new User(req.body);
   user.save(function(err, user) {
     res.render('dashboard', {
