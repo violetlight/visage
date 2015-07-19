a7e9e1d06e6a3225a27ffefa5d522c653c9727af
JF <jasonhfields@gmail.com>
2015-07-19 17:50:51 -0500

    move upload path to images/uploads and use a multer disk storage instead

--- routes/api.js
+++ routes/api.js
@@ -3,10 +3,17 @@
 var express = require("express");
 var router = express.Router();
 var multer = require("multer");
-var upload = multer({ dest: "./uploads/" });
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/images/uploads");
+  }
+});
+// var upload = multer({ dest: "./public/images" });
+var upload = multer({ storage: storage });
+
 
 router.post("/upload", upload.single("file"), function(req, res) {
-  console.dir(req.file);
+  // create new Image here and save it
   res.send("File uploaded successfully");
 });
 
