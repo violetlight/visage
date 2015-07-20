36e3ac41bb435fd16ca01f96defde4633cd1a84e
JF <jasonhfields@gmail.com>
2015-07-20 11:19:12 -0500

    Improved image route, slight changes to the dashboard view

--- routes/api.js
+++ routes/api.js
@@ -2,19 +2,38 @@
 
 var express = require("express");
 var router = express.Router();
+var Image = require('../models/image');
+var Album = require('../models/album');
+
 var multer = require("multer");
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/images/uploads");
   }
 });
-// var upload = multer({ dest: "./public/images" });
 var upload = multer({ storage: storage });
 
 
-router.post("/upload", upload.single("file"), function(req, res) {
+router.post("/image", upload.single("file"), function(req, res) {
   // create new Image here and save it
-  res.send("File uploaded successfully");
+  var fixedPath = req.file.path.replace("public/", "");
+  var image = new Image({
+    filepath: fixedPath,
+    viewableBy: [req.user._id] // the user that uploaded this file
+  });
+  image.save(function(err, image) {
+    // Associate Image with uploader
+    req.user.uploadedImages.push(image._id);
+    // …and save changes to the User.
+    req.user.save(function(err, user) {
+      res.send("it saved successfully");
+    });
+  });
+});
+
+router.post("/album", upload.array("files"), function(req, res) {
+  // create new Album here and save it
+  res.send("Files uploaded successfully");
 });
 
 
