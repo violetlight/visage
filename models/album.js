8c41461cf4e9d9e94ef18775e01523e648e4d2b6
JF <jasonhfields@gmail.com>
2015-07-19 19:50:55 -0500

    Album model

---
+++ models/album.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var mongoose = require("mongoose");
+
+var albumSchema = mongoose.Schema({
+  hash: String,
+  url: String,
+  private: {
+    type:Boolean,
+    default: false
+  },
+  images: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Image"
+  }]
+});
+
+module.exports = mongoose.model("Album", albumSchema);
