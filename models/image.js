b7b5259b00dbf943ad90095edfcdacad3ff39fb2
JF <jasonhfields@gmail.com>
2015-07-19 19:51:00 -0500

    Image model

---
+++ models/image.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var mongoose = require("mongoose");
+
+var imageSchema = mongoose.Schema({
+  url: String,
+  private: {
+    type:Boolean,
+    default: false
+  },
+  viewableBy: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }]
+});
+
+module.exports = mongoose.model("Image", imageSchema);
