d03c52ebe1bcafc0d0fa517853bce20c1bc8fe12
JF <jasonhfields@gmail.com>
2015-07-21 17:15:25 -0500

    Restructured the upload route a bit

--- routes/api.js
+++ routes/api.js
@@ -14,21 +14,23 @@ var storage = multer.diskStorage({
 var upload = multer({ storage: storage });
 
 /* POST '/image' - upload a single image file and create a new Image in db */
-router.post("/image", upload.single("file"), function(req, res) {
-  // create new Image here and save it
-  var fixedPath = req.file.path.replace("public/", "");
-  var image = new Image({
-    filepath: fixedPath,
-    viewableBy: [req.user._id], // the user that uploaded this file
-  });
-  image.save(function(err, image) {
-    // Associate Image with uploader
-    req.user.uploadedImages.push(image._id);
-    // …and save changes to the User.
-    req.user.save(function(err, user) {
-      res.send("it saved successfully");
+router.post("/image",
+  upload.single("file"),
+  function(req, res) {
+    // create new Image here and save it
+    var fixedPath = req.file.path.replace("public/", "");
+    var image = new Image({
+      filepath: fixedPath,
+      viewableBy: [req.user._id], // the user that uploaded this file
     });
-  });
+    image.save(function(err, image) {
+      // Associate Image with uploader
+      req.user.uploadedImages.push(image._id);
+      // …and save changes to the User.
+      req.user.save(function(err, user) {
+        res.send("it saved successfully");
+      });
+    });
 });
 
 router.post("/album", upload.array("files"), function(req, res) {
