bc261753a743710926b25c1d9790061e7f619588
JF <jasonhfields@gmail.com>
2015-07-21 13:42:20 -0500

    append uploader to viewableBy array on uploaded Images

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,8 @@
 
 var express = require("express");
 var router = express.Router();
-var Image = require('../models/image');
-var Album = require('../models/album');
+var Image = require("../models/image");
+var Album = require("../models/album");
 
 var multer = require("multer");
 var storage = multer.diskStorage({
@@ -19,7 +19,7 @@ router.post("/image", upload.single("file"), function(req, res) {
   var fixedPath = req.file.path.replace("public/", "");
   var image = new Image({
     filepath: fixedPath,
-    viewableBy: [req.user._id] // the user that uploaded this file
+    viewableBy: [req.user._id], // the user that uploaded this file
   });
   image.save(function(err, image) {
     // Associate Image with uploader
