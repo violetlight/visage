28679d0893f5e3b6b9bd675e3660e71e71fae670
JF <jasonhfields@gmail.com>
2015-07-21 17:03:04 -0500

    These urls are awesome (thanks Javad)

--- models/image.js
+++ models/image.js
@@ -2,6 +2,8 @@
 
 var mongoose = require("mongoose");
 
+var generateAAA = require("adjective-adjective-animal");
+
 var imageSchema = mongoose.Schema({
   filepath: String,
   hash: String,
@@ -14,5 +16,15 @@ var imageSchema = mongoose.Schema({
     ref: "User"
   }]
 });
+
+imageSchema.pre("save", function(next) {
+    var image = this;
+    generateAAA().then(function(aaa) {
+      // aaa is an adjective-adjective-animal "hash"
+      image.hash = aaa;
+      next();
+    });
+});
+
 
 module.exports = mongoose.model("Image", imageSchema);
