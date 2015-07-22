44003994bc781c79d016e8c3a7f6ce6edf41911e
JF <jasonhfields@gmail.com>
2015-07-21 22:29:04 -0500

    This thing has got to be solved, this is getting so old

--- app.js
+++ app.js
@@ -42,8 +42,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use("/api", api);
-app.use("/", routes);
 app.use("/users", users);
+app.use("/", routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
