69888c461d8c2b94c6599bfb94cfa42009aed2ea
JF <jasonhfields@gmail.com>
2015-07-22 18:01:22 -0500

    Restrict image uploading form to one at a time and delay uploading until button is clicked manually

--- public/javascripts/visage.js
+++ public/javascripts/visage.js
@@ -2,12 +2,38 @@
 
 var debugFunc = function(file, done) {
   console.log(file);
+  console.log("Debug function is being called at least");
   done();
 };
+
+var sendingCb = function(file, xhr, formData) {
+  formData.append("name", "bullshit");
+}
+
+var replaceOldWithNew = function(file) {
+  this.removeAllFiles();
+  this.addFile(file);
+}
+
+var init = function() {
+  var submitButton = document.querySelector("#submit-image"),
+    thisDropzone = this;
+
+    submitButton.addEventListener("click", function() {
+      thisDropzone.processQueue();
+    });
+
+}
+
 
 /* The form's id is actually `image-upload-form`, but
    I think Dropzone implicitly camelizes the identifier here. */
 Dropzone.options.imageUploadForm = {
   paramName: "file",
-  accept: debugFunc
+  maxFiles: 1,
+  maxfilesexceeded: replaceOldWithNew,
+  accept: debugFunc,
+  autoProcessQueue: false,
+  init: init,
+  sending: sendingCb
 };
